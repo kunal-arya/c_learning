6ffa00bf9a8bd767b55e5af9c8f1c9c942306c10
Kunal Arya <kunalaryad@gmail.com>
2024-10-22 07:22:59 +0530

    Array

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // Array = A Data Structure that can store many values of the same data types.
+
+    double prices[] = {10.34,54.76,32.43,654.4};
+
+    printf("%.1f",prices[1]);
+
+    return 0;
+}
